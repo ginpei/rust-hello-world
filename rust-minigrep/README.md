d55840ba9a73ecea25515fd3bf0fd99a2501a1d9
Ginpei <ginpei@ginpei.dev>
2022-07-31 14:01:03 -0700

    minigrep

---
+++ rust-minigrep/README.md
@@ -0,0 +1,3 @@
+# Rust - minigrep
+
+- [An I/O Project: Building a Command Line Program - The Rust Programming Language](https://doc.rust-lang.org/book/ch12-00-an-io-project.html)
---
+++ rust-minigrep/src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::process;
+
+use rust_minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // If missed `use std::process;`, error:
+    // > mismatched types
+    // > expected struct `Config`, found `()`rustc (E0308)
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    // Q. how to get Ok result?
+    if let Err(e) = rust_minigrep::run(config) {
+        println!("Application error: {}", e);
+        return;
+    }
+}
